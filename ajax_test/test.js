0b04d4872bbc2d04f60bc7c80db24dbb2f694f28
giogandola98 <giogandola@gmail.com>
2018-03-23 13:02:49 +0100

    nuovo

---
+++ ajax_test/test.js
@@ -0,0 +1,9 @@
+function print_form(segnati,subiti,occasioni)
+{
+  //console.log(document.getElementById("ciao").innerHTML="3");
+  alert("ciao");
+};
+function myfunction()
+{
+  alert("ciao");
+}
